7abdbcf1a57fd3cecc3f0577931faca14b932299
Oeoes Roy <oeoesroy@gmail.com>
2020-10-31 14:25:29 +0700

    printer using esc pos by mike42

--- app/Http/Controllers/Activity/PrintReceiptController.php
+++ app/Http/Controllers/Activity/PrintReceiptController.php
@@ -4,79 +4,24 @@ namespace App\Http\Controllers\Activity;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use charlieuki\ReceiptPrinter\ReceiptPrinter as ReceiptPrinter;
+use Mike42\Escpos\Printer;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
 class PrintReceiptController extends Controller
 {
     public function print_receipt () {
-        // Set params
-        $mid = '123123456';
-        $store_name = 'YOURMART';
-        $store_address = 'Mart Address';
-        $store_phone = '1234567890';
-        $store_email = 'yourmart@email.com';
-        $store_website = 'yourmart.com';
-        $tax_percentage = 10;
-        $transaction_id = 'TX123ABC456';
+        try {
+            $connector = new WindowsPrintConnector("namaprinter");
 
-        // Set items
-        $items = [
-            [
-                'name' => 'French Fries (tera)',
-                'qty' => 2,
-                'price' => 65000,
-            ],
-            [
-                'name' => 'Roasted Milk Tea (large)',
-                'qty' => 1,
-                'price' => 24000,
-            ],
-            [
-                'name' => 'Honey Lime (large)',
-                'qty' => 3,
-                'price' => 10000,
-            ],
-            [
-                'name' => 'Jasmine Tea (grande)',
-                'qty' => 3,
-                'price' => 8000,
-            ],
-        ];
-
-        // Init printer
-        $printer = new ReceiptPrinter;
-        $printer->init(
-            config('receiptprinter.connector_type'),
-            config('receiptprinter.connector_descriptor')
-        );
-
-        // Set store info
-        $printer->setStore($mid, $store_name, $store_address, $store_phone, $store_email, $store_website);
-
-        // Add items
-        foreach ($items as $item) {
-            $printer->addItem(
-                $item['name'],
-                $item['qty'],
-                $item['price']
-            );
+            /* Print a "Hello world" receipt" */
+            $printer = new Printer($connector);
+            $printer -> text("Hello World!\n");
+            $printer -> cut();
+            
+            /* Close printer */
+            $printer -> close();
+        } catch (\Throwable $th) {
+            echo "Couldn't print to this printer: " . $th -> getMessage() . "\n";
         }
-        // Set tax
-        $printer->setTax($tax_percentage);
-
-        // Calculate total
-        $printer->calculateSubTotal();
-        $printer->calculateGrandTotal();
-
-        // Set transaction ID
-        $printer->setTransactionID($transaction_id);
-
-        // Set qr code
-        $printer->setQRcode([
-            'tid' => $transaction_id,
-        ]);
-
-        // Print receipt
-        $printer->printReceipt();
     }
 }
