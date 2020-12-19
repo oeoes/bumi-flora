08f37c8d3c5eac7749d41dd5643d19c05fdd376b
Oeoes Roy <oeoesroy@gmail.com>
2020-12-19 19:41:27 +0700

    barcode feature

--- app/Http/Controllers/Activity/PrintBarcode.php
+++ app/Http/Controllers/Activity/PrintBarcode.php
@@ -5,13 +5,40 @@ namespace App\Http\Controllers\Activity;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Mike42\Escpos\Printer;
-use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
-use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 
 class PrintBarcode extends Controller
 {
-    public function print_barcode (Request $request) {
-        
+    public static function print_barcode () {
+        $connector = new WindowsPrintConnector("epson");
+
+        /* initiate printer */
+        $printer = new Printer($connector);
+        $printer->initialize();
+
+        /* Height and width */
+        $printer->selectPrintMode(Printer::MODE_DOUBLE_HEIGHT | Printer::MODE_DOUBLE_WIDTH);
+        $printer->text("Height and bar width\n");
+        $printer->selectPrintMode();
+        $heights = array(1, 2, 4, 8, 16, 32);
+        $widths = array(1, 2, 3, 4, 5, 6, 7, 8);
+        $printer->text("Default look\n");
+        $printer->barcode("ABC", Printer::BARCODE_CODE39);
+        foreach ($heights as $height) {
+            $printer->text("\nHeight {$height}\n");
+            $printer->setBarcodeHeight($height);
+            $printer->barcode("ABC", Printer::BARCODE_CODE39);
+        }
+        foreach ($widths as $width) {
+            $printer->text("\nWidth {$width}\n");
+            $printer->setBarcodeWidth($width);
+            $printer->barcode("ABC", Printer::BARCODE_CODE39);
+        }
+        $printer->feed();
+        // Set to something sensible for the rest of the examples
+        $printer->setBarcodeHeight(40);
+        $printer->setBarcodeWidth(2);
+        /* Text position */
+        $printer->selectPrintMode(Printer::MODE_DOUBLE_HEIGHT | Printer::MODE_DOUBLE_WIDTH);
     }
 }
--- app/Http/Controllers/Admin/BarcodeGenerator.php
+++ app/Http/Controllers/Admin/BarcodeGenerator.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Activity\PrintBarcode;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -27,15 +28,20 @@ class BarcodeGenerator extends Controller
     }
 
     public function print_barcode (Request $request) {
-        $item = DB::table('items')->select('name', 'barcode', 'price')->where('id', request('item_id'))->first();
-        $barcode_img = DNS1D::getBarcodePNG($item->barcode, 'C39');
-
         try {
-            \File::put(storage_path('/app/barcodes') . '/' . 'barcode.png', base64_decode($barcode_img));
-            return response()->json(['status' => true, 'message' => 'barcode stored']);
+            PrintBarcode::print_barcode();
         } catch (\Throwable $th) {
-            return response()->json(['status' => false, 'message' => 'error: ' . $th], 400);
+            echo $th->getMessage();
         }
+        // $item = DB::table('items')->select('name', 'barcode', 'price')->where('id', request('item_id'))->first();
+        // $barcode_img = DNS1D::getBarcodePNG($item->barcode, 'C39');
+
+        // try {
+        //     \File::put(storage_path('/app/barcodes') . '/' . 'barcode.png', base64_decode($barcode_img));
+        //     return response()->json(['status' => true, 'message' => 'barcode stored']);
+        // } catch (\Throwable $th) {
+        //     return response()->json(['status' => false, 'message' => 'error: ' . $th], 400);
+        // }
         
     }
 }
