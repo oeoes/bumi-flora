b0451aa3abc88f986ebfbb4ba342ed3c79d86abe
Oeoes Roy <oeoesroy@gmail.com>
2020-12-26 17:37:23 +0700

    kebalik

--- app/Exports/MasterDataExport.php
+++ app/Exports/MasterDataExport.php
@@ -25,12 +25,12 @@ class MasterDataExport implements FromCollection, WithHeadings
             ->join('stocks', 'stocks.item_id', '=', 'items.id')
             ->leftJoin('stake_holders', 'stake_holders.id', '=', 'items.stake_holder_id')
             ->where('stocks.dept', $this->dept)
-            ->select('items.barcode as item_code', 'items.barcode', 'items.name as item', 'categories.category', 'units.unit', 'brands.brand', 'items.base_unit', 'items.base_unit_conversion', 'items.main_cost', 'items.price', DB::raw('IFNULL(stocks.amount, 0) as stock'), 'items.min_stock', 'categories.id as item_type', 'items.base_unit_conversion as using_serial', 'stocks.dept', 'items.cabinet', 'stake_holders.name', 'items.description')
+            ->select('items.barcode as item_code', 'items.barcode', 'items.name as item', 'categories.category', 'units.unit', 'brands.brand', 'items.base_unit', 'items.base_unit_conversion', 'items.main_cost', 'items.price', DB::raw('IFNULL(stocks.amount, 0) as stock'), 'items.min_stock', 'categories.id as item_type', 'items.base_unit_conversion as using_serial', 'items.cabinet', 'stocks.dept', 'stake_holders.name', 'items.description')
             ->get();
     }
 
     public function headings(): array
     {
-        return ["Kode Item", "Barcode", "Nama Item", "Jenis", "Satuan", "Merk", "Satuan Dasar", "Konversi satuan dasar", "Harga pokok", "Harga jual", "Stok", "Stok Min.", "Tipe Item", "Menggunakan Serial", "Kode Gudang - Kantor", "Rak", "Supplier", "Keterangan"];
+        return ["Kode Item", "Barcode", "Nama Item", "Jenis", "Satuan", "Merk", "Satuan Dasar", "Konversi satuan dasar", "Harga pokok", "Harga jual", "Stok", "Stok Min.", "Tipe Item", "Menggunakan Serial",  "Rak", "Kode Gudang - Kantor", "Supplier", "Keterangan"];
     }
 }
