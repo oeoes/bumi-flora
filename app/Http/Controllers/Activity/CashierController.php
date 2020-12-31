69593f752becbdee8ec6577d43f508922beb7f51
hidayahhk <hidayahhusnul.khotimah@yahoo.com>
2020-12-31 10:36:20 +0700

    local

--- app/Http/Controllers/Activity/CashierController.php
+++ app/Http/Controllers/Activity/CashierController.php
@@ -241,7 +241,6 @@ class CashierController extends Controller
                     "cashback" => $request->payment_type != 1 ? '-' : number_format($request->nominal - $bill),
                     "transaction_number" => $trx_number . '/' . Carbon::now()->format('Y-m-d')
                 ];
-                dd($calc);
                 // print receipt
                 PrintReceiptController::print_receipt($print_items, $calc);
             }
